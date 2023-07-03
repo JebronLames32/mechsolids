46f6a0b25579404aa786da86ab0002e0e92f8a63
JebronLames32 <abhinavcillanki@gmail.com>
2023-07-04 01:57:42 +0530

    RC Hibbeler problem solution added

--- RCHibb-6.45.py
+++ RCHibb-6.45.py
@@ -1,19 +1,21 @@
 from SimplySupportedBeam import *
 from math_functions import *
 
-Name = "Q12"
-L = 4
+Name = "6.45"
+L = 1
 Beam1 = SimplySupportedBeam(Name, L)
-equation = "w0+k*x**2"
-load_list = [EquationLoad(equation,0,L,2.4,4.8)]
-support = Support(0,4)
+# equation is w0*x**2/L**2 w0 is taken as unity and L is taken as 1 arbitrarily
+# change it according to the question
+equation = "(x**2)/1"
+load_list = [EquationLoad(equation,0,L,0,1)]
+support = Support(0,1)
 
 
 for load in load_list :
     Beam1.add_load(load)
     print(load.equation)
 Beam1.add_supports(support)
-Beam1.generate_sf_at_all_points()
+# Beam1.generate_sf_at_all_points()
 # print(Beam1.supports[0].reaction1)
 
 Beam1.plot_sfd()
